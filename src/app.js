a147148b7f4fae75e4eba6b27775f69a025bfdd0
Julio Cesar <juliocesartrujillolugo@gmail.com>
2025-10-20 19:25:48 +0000

    CARTAS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-import "bootstrap";
-import "./style.css";
+const VALUE = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const SYMBOL = ["♥", "♦", "♠", "♣"]; 
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateRandomCard() {
+    const value = getRandomElement(VALUE);
+    const symbol = getRandomElement(SYMBOL);
+
+    const isRedSymbol = (symbol === "♥" || symbol === "♦");
+    const symbolColorClass = isRedSymbol ? "red-symbol" : "black-symbol";
+
+    document.getElementById('value-top').textContent = value;
+    document.getElementById('symbol-top').textContent = symbol;
+
+    document.getElementById('value-center').textContent = value;
+
+    document.getElementById('symbol-bottom').textContent = symbol;
+    document.getElementById('value-bottom').textContent = value;
+
+    const elementsToColor = [
+        document.getElementById('value-top'),
+        document.getElementById('symbol-top'),
+        document.getElementById('value-center'),
+        document.getElementById('symbol-bottom'),
+        document.getElementById('value-bottom')
+    ];
+
+    elementsToColor.forEach(el => {
+        el.classList.remove('red-symbol','black-symbol'); 
+        el.classList.add(symbolColorClass);
+    });
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    generateRandomCard(); 
+
+    document.getElementById('generate-button').addEventListener('click', generateRandomCard);
 };
